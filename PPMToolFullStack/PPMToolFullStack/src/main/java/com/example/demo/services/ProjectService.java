8b8dbd8c3d2e3cd6315d0dfbf1e90c46fc07b132
Adityaverma123 <av9567@gmail.com>
2021-06-07 13:05:51 +0530

    unique identifier validation added

--- PPMToolFullStack/PPMToolFullStack/src/main/java/com/example/demo/services/ProjectService.java
+++ PPMToolFullStack/PPMToolFullStack/src/main/java/com/example/demo/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import com.example.demo.domain.Project;
+import com.example.demo.exceptions.ProjectIdException;
 import com.example.demo.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+
+        }
+        catch (Exception e){
+            throw new ProjectIdException("ProjectId '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
